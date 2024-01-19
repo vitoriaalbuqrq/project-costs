5a619bee114130627bd94557b8ed5ae747cc6005
Vitoria Albuquerque <albuquerquevitoria14@gmail.com>
2024-01-18 21:19:39 -0300

    Implementação das páginas Home e Projects com CRUD

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import Container from "./Container";
+import styles from "./Navbar.module.css";
+import logo from "../../img/costs_logo.png";
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <Container>
+        <Link to="/">
+          <img src={logo} alt="Costs" />
+        </Link>
+        <ul className={styles.list}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/company">Empresa</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projetos</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contato</Link>
+          </li>
+        </ul>
+      </Container>
+    </nav>
+  );
+}
+export default Navbar;
